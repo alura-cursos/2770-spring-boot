a633d39e3453ec615cfa3cc86d98440092eebec7
Rodrigo Caneppele <r.caneppele@gmail.com>
2022-10-20 17:05:20 -0300

    Detalhando dados na API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -53,5 +53,11 @@ public class MedicoController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
 
 }
