60831c0fd98331640df4e36c79dfa05afae0d184
Rodrigo Caneppele <r.caneppele@gmail.com>
2022-10-24 16:47:31 -0300

    Repository e Service

---
+++ src/main/java/med/voll/api/domain/usuario/AutenticaoService.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
